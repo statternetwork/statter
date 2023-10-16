3a78dd645f2fa7e2f11fb6b423a49e4e96f9a090
statternetwork <121543894+statternetwork@users.noreply.github.com>
2023-10-16 09:19:38 +0800

    Update PromotionLedgerMapper.java

--- common/base/src/main/java/com/synctech/statter/base/mapper/PromotionLedgerMapper.java
+++ common/base/src/main/java/com/synctech/statter/base/mapper/PromotionLedgerMapper.java
@@ -13,34 +13,34 @@ import static com.synctech.statter.constant.Constant.WALLET_ADDRESS_LENGTH;
 @Repository
 public interface PromotionLedgerMapper {
 
-    @Select("SELECT count(*) FROM information_schema.TABLES WHERE table_name='ledger_${promotion}'")
+    @Select("SELECT count(*) FROM information_schema.TABLES WHERE table_name='ledger_pro'")
     int existTable(@Param("promotion") String promotion);
 
-    @Update("DROP TABLE IF EXISTS ledger_${promotion}")
+    @Update("DROP TABLE IF EXISTS ledger_pro")
     int dropTable(@Param("promotion") String promotion);
 
-    @Update("CREATE TABLE ledger_${promotion} (blockIndex bigint(20) NOT NULL,createdTime timestamp NOT NULL," +
+    @Update("CREATE TABLE ledger_pro (blockIndex bigint(20) NOT NULL,createdTime timestamp NOT NULL," +
             "sn char(32) NOT NULL,address char(" + WALLET_ADDRESS_LENGTH + ") NOT NULL,mingProfit varchar(255),data longblob NOT NULL,PRIMARY KEY (blockIndex))")
     int createNewTable(@Param("promotion") String promotion);
 
-    @Insert("INSERT INTO ledger_${promotion} (blockIndex, createdTime, sn, address, mingProfit, data) " +
+    @Insert("INSERT INTO ledger_pro (blockIndex, createdTime, sn, address, mingProfit, data) " +
             "VALUES (#{ledger.blockIndex},#{ledger.createdTime},#{ledger.sn},#{ledger.address},#{ledger.mingProfit},#{ledger.data})")
     int insert(@Param("promotion") String promotion, @Param("ledger") Ledger ledger);
 
-    // @Select("SELECT COUNT(`blockIndex`) FROM ledger_${promotion};")
-    @Select("SELECT TABLE_ROWS FROM information_schema.TABLES WHERE table_name='ledger_${promotion}'")
+    // @Select("SELECT COUNT(`blockIndex`) FROM ledger_pro;")
+    @Select("SELECT TABLE_ROWS FROM information_schema.TABLES WHERE table_name='ledger_pro'")
     Long count(@Param("promotion") String promotion);
 
-    @Select("SELECT * FROM ledger_${promotion} WHERE blockIndex=#{bi};")
+    @Select("SELECT * FROM ledger_pro WHERE blockIndex=#{bi};")
     Ledger findByBlockIndex(@Param("promotion") String promotion, @Param("bi") long blockIndex);
 
-    @Select("SELECT * FROM ledger_${promotion} WHERE `blockIndex`>#{si} and `blockIndex`<#{ei} ORDER BY `blockIndex` DESC;")
+    @Select("SELECT * FROM ledger_pro WHERE `blockIndex`>#{si} and `blockIndex`<#{ei} ORDER BY `blockIndex` DESC;")
     List<Ledger> findByBlockIndexArea(@Param("promotion") String promotion, @Param("si") long si, @Param("ei") long ei);
 
-    @Select("SELECT * FROM ledger_${promotion} ORDER BY `blockIndex` DESC limit #{lm};")
+    @Select("SELECT * FROM ledger_pro ORDER BY `blockIndex` DESC limit #{lm};")
     List<Ledger> findLimit(@Param("promotion") String promotion, @Param("lm") int lm);
 
-    @Select("SELECT * FROM ledger_${promotion} ORDER BY `blockIndex` ASC LIMIT #{startIndex},#{endIndex};")
+    @Select("SELECT * FROM ledger_pro ORDER BY `blockIndex` ASC LIMIT #{startIndex},#{endIndex};")
     List<Ledger> page(@Param("promotion") String promotion, @Param("startIndex") long startIndex, @Param("endIndex") long endIndex);
 
 }
